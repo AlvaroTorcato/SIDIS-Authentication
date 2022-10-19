1ff6db1e5a7cc639428d2cb7386b211f372690bb
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-10-19 09:43:24 +0100

    Initial commit

---
+++ src/main/java/com/example/sidisauthentication/SidisAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sidisauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidisAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidisAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sidisauthentication/SidisAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidisauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidisAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
