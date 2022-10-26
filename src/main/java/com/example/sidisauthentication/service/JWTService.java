25023afcd7d4b72b83fd5a639b30dc5f09bfc20b
TiagoNora <1201050@isep.ipp.pt>
2022-10-26 08:28:49 +0100

    Alteração do JWTService para verificaçao do usuario

--- src/main/java/com/example/sidisauthentication/service/JWTService.java
+++ src/main/java/com/example/sidisauthentication/service/JWTService.java
@@ -2,7 +2,9 @@ package com.example.sidisauthentication.service;
 
 import com.example.sidisauthentication.model.JWT;
 import com.example.sidisauthentication.model.LoginRequest;
+import com.example.sidisauthentication.model.UserDTO;
 import com.example.sidisauthentication.repository.JWTRepository;
+import com.example.sidisauthentication.repository.UserRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +23,25 @@ public class JWTService {
     @Value("${sidis.secret}")
     private String secret;
     @Autowired
-    private JWTRepository repository;
+    private JWTRepository jwtRepository;
+
+    @Autowired
+    private UserRepository userRepository;
     public String createJWT(LoginRequest loginRequest){
         String string = generate(loginRequest);
         JWT jwt = new JWT(string);
-        repository.save(jwt);
+        jwtRepository.save(jwt);
         return string;
     }
 
     public String generate(LoginRequest loginRequest){
-
+        UserDTO user = userRepository.findUserByUsername(loginRequest.getUsername());
         Key hmacKey = new SecretKeySpec(Base64.getDecoder().decode(secret),
                 SignatureAlgorithm.HS256.getJcaName());
         Instant now = Instant.now();
         String jwtToken = Jwts.builder()
                 .claim("username", loginRequest.getUsername())
-                .claim("email", "jane@example.com")
+                .claim("roles", user.getRoles())
                 .setIssuedAt(Date.from(now))
                 .setExpiration(Date.from(now.plus(30l, ChronoUnit.MINUTES)))
                 .signWith(hmacKey)
